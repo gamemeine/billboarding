71e60e0b4e69d4d8dd72dc1fc85740afb18e2577
grzegorzstatkiewicz <grzegorz.statkiewicz@gmail.com>
2025-05-11 22:24:04 +0200

    Add support for Mac

--- src/Window.cpp
+++ src/Window.cpp
@@ -31,7 +31,12 @@ bool Window::Init(int width, int height, const std::string& title)
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);    
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    // Enable forward‐compat on macOS
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     _window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
     if (!_window)
